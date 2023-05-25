47c2b9ff75d6c016f9cd1e5da799651e5c56fff7
Sheida Alfaro Taco <alfarosh@uoc.edu>
2023-05-25 21:39:50 +0200

    remove lodash

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,3 @@
-import _ from "lodash";
-
-console.log(_.concat([10,15,20],[40]));
-
 let map = L.map('map').setView([41.3624, 2.1065], 12);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
